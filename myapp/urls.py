a51229e014ebe8cbb884c713a1c6dd36b5308fe9
NdelogakehDaniel <ndabosed@gmail.com>
2021-12-27 14:02:21 +0100

    Django todolist application

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('',views.add),
     path('add/',views.add,name="add_list"),
     path('update/<int:id>',views.update,name="update_list"),
     path('delete/<int:id>',views.delete,name="delete_list"),
